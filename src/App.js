85511cad6b2a40919434cd6e75d5d253caf237c9
ajiiz <opticpiter@gmail.com>
2021-07-16 13:25:39 +0200

    Modified paths

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom"
 
+import Navbar from './components/Navbar'
 import Article from './components/Article'
 import Articles from './components/Articles'
+import FavouriteArticles from './components/FavouriteArticles'
 import "./index.css"
 
 const App = () => {
 	return (
 		<Router>
+			<Navbar />
       <Switch>
         <Route path="/articles" component={Articles} />
+				<Route path="/favourite" component={FavouriteArticles} />
 				<Route exact path="/articles/:id" component={Article} />
         <Route path="/"><Redirect to="/articles" /></Route>
       </Switch>
---
+++ src/components/FavouriteArticles/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FavouriteArticles = () => {
+	return (
+		<div>
+			Favourite Articles
+		</div>
+	)
+}
+
+export default FavouriteArticles
