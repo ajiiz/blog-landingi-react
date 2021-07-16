e0300fd32cea77d34a9cd572ecc982151a504950
ajiiz <opticpiter@gmail.com>
2021-07-16 12:14:53 +0200

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# Blog-Landingi
+
+## Table of contents
+* [Status](#status)
+* [Technologies](#technologies)
+* [Setup](#setup)
+* [Contact](#contact)
+
+## Status
+
+## Technologies
+
+## Setup
+
+## Contact
+#### Contact me via e-mail: piotrwrobel.ajiiz@gmail.com
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HTMLwebpackplugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      { test: /\.jsx?$/, loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      { test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      }
+    ]
+  },
+  plugins: [
+    new HTMLwebpackplugin({
+      template: "./public/index.html"
+    })
+  ]
+};
