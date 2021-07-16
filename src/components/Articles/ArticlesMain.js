c4a9fc5bca36d5a3bf6a3c74d2fb286bef16d743
ajiiz <opticpiter@gmail.com>
2021-07-16 20:23:14 +0200

    Added ArticlesMain component that is displaying a big test article

--- src/components/Articles/ArticlesMain.js
+++ src/components/Articles/ArticlesMain.js
@@ -1,15 +1,30 @@
 import React from 'react'
 
+import photo from "../../assets/header-photo.jpg"
+
 const ArticlesMain = ({ styles }) => {
 	return (
-		<div className={styles.container}>
-			<h1 className={styles.header}>Articles & News</h1>
-			<p className={styles.info}>
-				<span className={styles.text}>Create your own content.</span>
-				<span className={styles.text}>Look for inspirations and knowledge.</span>
-				<span className={styles.text}>Follow your favourites.</span>
-			</p>
-		</div>
+		<>
+			<div className={styles.container}>
+				<h1 className={styles.header}>Articles & News</h1>
+				<p className={styles.info}>
+					<span>Create your own content.</span>
+					<span>Look for inspirations and knowledge.</span>
+					<span>Follow your favourites.</span>
+				</p>
+			</div>
+			<div className={styles.mainarticle}>
+				<img src={photo} className={styles.photo} />
+				<div className={styles.content}>
+					<h3 className={styles.title}>There might be <span>Your</span> title</h3>
+					<p className={styles.text}>
+						<span>There would go your content...</span>
+						<span>There would go your content...</span>
+						<span>There would go your content...</span>
+					</p>
+				</div>
+			</div>
+		</>
 	)
 }
 
