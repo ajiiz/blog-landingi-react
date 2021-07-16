fe829986de971f46878b842a5fdb7b70ef12f6db
ajiiz <opticpiter@gmail.com>
2021-07-16 16:48:09 +0200

    Imported and modified child components structure

--- src/components/Articles/index.js
+++ src/components/Articles/index.js
@@ -1,8 +1,14 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { fetchArticles } from '../services/articles'
 
+import ArticlesShowcase from './ArticlesShowcase'
+import ArticlesMain from './ArticlesMain'
+import styles from "./articles.module.css"
+
 const Articles = () => {
+
+	const [articles, setArticles] = useState({})
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -17,8 +23,9 @@ const Articles = () => {
 	}, [])
 
 	return (
-		<div>
-				Articles! Articles! Articles!
+		<div className={styles.articles}>
+			<ArticlesMain styles={styles} />
+			<ArticlesShowcase styles={styles} />
 		</div>
 	)
 }
