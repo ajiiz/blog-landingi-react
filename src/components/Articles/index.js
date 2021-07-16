01fe172352a3db4b4caf371d8bdf1035b4a312ef
ajiiz <opticpiter@gmail.com>
2021-07-16 17:19:13 +0200

    Fixed the unmounted component warning

--- src/components/Articles/index.js
+++ src/components/Articles/index.js
@@ -11,21 +11,26 @@ const Articles = () => {
 	const [articles, setArticles] = useState({})
 
 	useEffect(() => {
+		let isMounted = true;
 		const fetchData = async () => {
 			try {
 				const data = await fetchArticles()
-				console.log(data)
+				if (isMounted) {
+					setArticles(data)
+				}
+				console.log(data.slice(0,10))
 			} catch(error) {
 				console.log(`Couldnt fetch the posts! Error message:${error}`)
 			}
 		}
 		fetchData()
+		return () => { isMounted = false }
 	}, [])
 
 	return (
 		<div className={styles.articles}>
 			<ArticlesMain styles={styles} />
-			<ArticlesShowcase styles={styles} />
+			<ArticlesShowcase styles={styles} data={articles}/>
 		</div>
 	)
 }
