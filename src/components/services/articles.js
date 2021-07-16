c55bd6307c1d9602164104fc5c90d08b46bfacf4
ajiiz <opticpiter@gmail.com>
2021-07-16 15:00:46 +0200

    Modified the fetchArticle

--- src/components/services/articles.js
+++ src/components/services/articles.js
@@ -12,7 +12,8 @@ export const fetchArticle = async (articleId) => {
     try {
         const response = await fetch("https://jsonplaceholder.typicode.com/posts")
         const data = await response.json()
-        return data.filter((elem) => elem.id === articleId)
+        let article = data.filter((elem) => elem.id === articleId)
+        return article
     } catch (error) {
         throw Error(`Couldnt fetch the posts! Error message:${error}`)
     }
