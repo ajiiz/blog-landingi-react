d9d9d3c1c202dbb5f471449a82ac5cdc44eb257e
ajiiz <opticpiter@gmail.com>
2021-07-16 15:01:22 +0200

    Added method to fetch user by id

---
+++ src/components/services/users.js
@@ -0,0 +1,10 @@
+export const fetchUser = async (userId) => {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users")
+        const data = await response.json()
+        let user = data.filter((elem) => elem.id === userId)
+        return user
+    } catch (error) {
+        throw Error(`Couldnt fetch the posts! Error message:${error}`)
+    }
+}
