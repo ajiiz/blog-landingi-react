798b60072bcaf4e3568aff23836e7f6c1a727ce5
ajiiz <opticpiter@gmail.com>
2021-07-17 19:29:31 +0200

    Fixed fetch methods in services

--- src/components/services/articles.js
+++ src/components/services/articles.js
@@ -15,8 +15,8 @@ export const fetchArticle = async (articleId) => {
     try {
         const response = await fetch("https://jsonplaceholder.typicode.com/posts")
         const data = await response.json()
-        let article = data.filter((elem) => elem.id === articleId)
-        return article
+        let article = data.filter((elem) => elem.id == articleId)
+        return article[0]
     } catch (error) {
         throw Error(`Couldnt fetch the post! Error message:${error}`)
     }
--- src/components/services/users.js
+++ src/components/services/users.js
@@ -2,8 +2,8 @@ export const fetchUser = async (userId) => {
     try {
         const response = await fetch("https://jsonplaceholder.typicode.com/users")
         const data = await response.json()
-        let user = data.filter((elem) => elem.id === userId)
-        return user
+        let user = data.filter((elem) => elem.id == userId)
+        return user[0]
     } catch (error) {
         throw Error(`Couldnt fetch the user! Error message:${error}`)
     }
