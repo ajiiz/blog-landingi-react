4e38cb7cb8c8b39003c9680c7dc33932c26ef32a
ajiiz <opticpiter@gmail.com>
2021-07-17 17:31:13 +0200

    Added Footer Component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import styles from "./footer.module.css"
+
+const Footer = () => {
+	return (
+		<div className={styles.footer}>
+			<a href="https://github.com/ajiiz" target="_blank" rel="noreferrer" className={styles.footer__text}>
+				© 2021 ajiiz.piotrwrobel
+      </a>
+		</div>
+	)
+}
+
+export default Footer
