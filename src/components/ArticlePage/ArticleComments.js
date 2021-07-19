0f05146a7fa2f4b3c0d683cd4380c51c8e17ab4e
ajiiz <opticpiter@gmail.com>
2021-07-19 22:39:38 +0200

    Added ArticleComments elements

--- src/components/ArticlePage/ArticleComments.js
+++ src/components/ArticlePage/ArticleComments.js
@@ -1,18 +1,20 @@
 import React from 'react'
 
 import ArticleComment from "./ArticleComment"
+import styles from "./article.module.css"
 
 const ArticleComments = ({ comments }) => {
 	return (
-		<>
-		{
-			comments.map((data, key) => {
-				return (
-					<ArticleComment comment={data} key={key} />
-				)
-			})
-		}
-		</>
+		<div className={styles.comment__container}>
+			<h3 className={styles.comment__header}>Comment Section</h3>
+			{
+				comments.map((data, key) => {
+					return (
+						<ArticleComment comment={data} key={key} />
+					)
+				})
+			}
+		</div>
 	)
 }
 
