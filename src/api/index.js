28a47c9b0a8a619ab94898aacafcaa4e09c695bd
ajiiz <opticpiter@gmail.com>
2021-07-20 16:22:55 +0200

    Added fetchArticle in api

--- src/api/index.js
+++ src/api/index.js
@@ -10,3 +10,15 @@ export const fetchArticles = async (page) => {
         throw Error(`Couldnt fetch the posts! Error message:${error}`)
     }
 }
+
+
+export const fetchArticle = async (articleId) => {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+        const data = await response.json()
+        let article = data.filter((elem) => elem.id == articleId)
+        return article[0]
+    } catch (error) {
+        throw Error(`Couldnt fetch the post! Error message:${error}`)
+    }
+}
