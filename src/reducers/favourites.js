c7df96d423901b7cc0aaee6ae283414adb52dbc4
ajiiz <opticpiter@gmail.com>
2021-07-20 22:14:21 +0200

    Added fetchAllArticles in favourites reducer

--- src/reducers/favourites.js
+++ src/reducers/favourites.js
@@ -2,6 +2,8 @@ const favouritesReducer =  (favourites = [], action) => {
     switch (action.type) {
         case "FETCH_ALL_IDS":
             return action.payload
+        case "FETCH_ALL_ARTICLES":
+            return action.payload
         case "ADD":
             return action.payload
         case "REMOVE":
