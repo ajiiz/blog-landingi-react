f915df1ec6bc73623cbbb545be1c19245bda59fd
ajiiz <opticpiter@gmail.com>
2021-07-20 22:15:39 +0200

    Added FavouriteArticlesPage elements

--- src/components/FavouriteArticlesPage/index.js
+++ src/components/FavouriteArticlesPage/index.js
@@ -1,16 +1,32 @@
 import React, { useEffect } from 'react'
+import { useDispatch } from "react-redux"
+import { getFavouriteArticles } from "../../actions/favourites"
+import { useSelector } from "react-redux"
+import ArticleShowcase from "../ArticlesPage/ArticlesShowcase"
 
 const FavouriteArticlesPage = () => {
+
+	const favouriteArticles = useSelector((state) => state.favourites)
+
+	const dispatch = useDispatch()
 
 	useEffect(() => {
 		window.scrollTo(0, 0)
 	}, [])
 
+	useEffect(() => {
+		dispatch(getFavouriteArticles())
+	}, [dispatch])
+
 	return (
-		<div>
-			Favourite Articles
-		</div>
-	)
+		<>
+			{
+				favouriteArticles.length > 0 ?
+					<ArticleShowcase articles={favouriteArticles} />
+				:	<h3>You dont have any favourites</h3>
+			}
+		</>
+		)
 }
 
 export default FavouriteArticlesPage
