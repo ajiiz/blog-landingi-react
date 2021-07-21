1f7ecb2e6b9d2315067f037082693b9e4145bb6b
ajiiz <opticpiter@gmail.com>
2021-07-21 16:23:57 +0200

    Added Footer tests

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,10 +4,10 @@ import styles from "./footer.module.css"
 
 const Footer = () => {
 	return (
-		<div className={styles.footer}>
-			<a href="https://github.com/ajiiz" target="_blank" rel="noreferrer" className={styles.footer__text}>
+		<div className={styles.footer} data-testid="footer-1" >
+			<a data-testid="footer-2" href="https://github.com/ajiiz" target="_blank" rel="noreferrer" className={styles.footer__text}>
 				© 2021 ajiiz.piotrwrobel
-      </a>
+      		</a>
 		</div>
 	)
 }
