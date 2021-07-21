04bc62d7427cb35edd312ecc2759a2757c3af521
ajiiz <opticpiter@gmail.com>
2021-07-21 11:54:05 +0200

    Modified CommentForm to controlled component

--- src/components/ArticlePage/CommentForm.js
+++ src/components/ArticlePage/CommentForm.js
@@ -1,19 +1,44 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 
 import styles from "./article.module.css"
 
 const CommentForm = () => {
+
+	const [userComment, setUserComment] = useState({name: "", content: ""})
+
+	const handleChange = (event) => {
+		const { name, value } = event.target
+		setUserComment({...userComment, [name]: value })
+	}
+
+	const handleSubmit = () => {
+
+	}
+
 	return (
 		<>
 			<h3 className={styles.comment__header}>Add comment</h3>
-			<form className={styles.comment__form}>
+			<form className={styles.comment__form} onSubmit={handleSubmit}>
         <label>
           <div className={styles.form__text}>Name</div>
-          <input className={styles.form__name} type="text" placeholder="your name"/>
+          <input
+						className={styles.form__name}
+						type="text"
+						placeholder="your name"
+						name="name"
+						value={userComment.name}
+						onChange={handleChange}
+					/>
         </label>
 				<label>
 					<div className={styles.form__text}>Comment</div>
-          <textarea className={styles.form__comment} placeholder="your comment"/>
+          <textarea
+						className={styles.form__comment}
+						placeholder="your comment"
+						name="content"
+						value={userComment.content}
+						onChange={handleChange}
+					/>
         </label>
         <input type="submit" value="send" />
       </form>
