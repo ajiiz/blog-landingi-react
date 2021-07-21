390332cffb62d8d8becb590e2afa573f04250cb7
ajiiz <opticpiter@gmail.com>
2021-07-21 12:36:14 +0200

    Added ADD action type in comments reducer

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -2,6 +2,8 @@ const commentsReducer =  (comments = [], action) => {
     switch (action.type) {
         case "FETCH_ALL":
             return action.payload
+        case "ADD":
+            return [...comments, action.payload]
         default:
             return comments
     }
